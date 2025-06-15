5c1e90cbcedb579f305324ded6aea91af643f412
Lahiru Peiris <lahirupeiris88@gmail.com>
2025-06-15 22:25:17 +0100

    Add project files.

---
+++ SachisGalleryWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SachisGalleryWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
